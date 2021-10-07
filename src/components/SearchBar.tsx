433a3d1d705063edd2ef469e8737f012480f8462
Chimson <crunor55@hotmail.com>
2021-10-07 11:54:23 +0200

    add unsplashConfig file and api request to handleSubmit

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,22 +2,33 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { useAppDispatch, useAppSelector } from "../app/hooks";
 import { setQuery } from "../features/query/querySlice";
+import { unsplash } from "../unsplashConfig";
 
 export function SearchBar() {
   const query = useAppSelector((state) => state.query.value);
+
   const dispatch = useAppDispatch();
+
   function handleSubmit(e: React.BaseSyntheticEvent) {
+    unsplash.search
+      .getPhotos({ query })
+      .then((photos) => console.log(photos))
+      .catch((error) => console.log(error));
     e.preventDefault();
   }
+
   function handleChange(e: React.BaseSyntheticEvent) {
     dispatch(setQuery(e.target.value));
   }
 
   return (
     <Form className="mt-5" onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="formQuery">
-        <Form.Label>Search for image</Form.Label>
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="queryInput">
+          Search for an Unsplash image
+        </Form.Label>
         <Form.Control
+          id="queryInput"
           type="query"
           placeholder="Type to search"
           value={query}
