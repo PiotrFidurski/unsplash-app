bc08e1e26fd1c218ee12174b6e2e0054e4dcd033
Chimson <crunor55@hotmail.com>
2021-10-07 11:42:41 +0200

    add querySlice to redux

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,12 +1,28 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import { setQuery } from "../features/query/querySlice";
 
 export function SearchBar() {
+  const query = useAppSelector((state) => state.query.value);
+  const dispatch = useAppDispatch();
+  function handleSubmit(e: React.BaseSyntheticEvent) {
+    e.preventDefault();
+  }
+  function handleChange(e: React.BaseSyntheticEvent) {
+    dispatch(setQuery(e.target.value));
+  }
+
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form className="mt-5" onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="formQuery">
         <Form.Label>Search for image</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control
+          type="query"
+          placeholder="Type to search"
+          value={query}
+          onChange={handleChange}
+        />
       </Form.Group>
       <Button variant="primary" type="submit">
         Search
