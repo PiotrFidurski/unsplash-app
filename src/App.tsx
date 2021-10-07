ef42b41f79387d78262eaf7baf68604d3dd97048
Chimson <crunor55@hotmail.com>
2021-10-07 19:33:08 +0200

    add useEffect to presist data to localStorage so its presisted on every route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import * as React from "react";
 import { Container, Row } from "react-bootstrap";
+import { useAppSelector } from "./app/hooks";
 import { Routes } from "./components/Routes";
 
 function App() {
+  const folders = useAppSelector((state) => state.folders.folders);
+
+  React.useEffect(() => {
+    localStorage.setItem("folders", JSON.stringify({ folders }));
+  }, [folders]);
+
   return (
     <Container fluid>
       <Row>
