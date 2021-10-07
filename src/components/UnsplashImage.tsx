4e786c1e9be8ffb812a38d7d9d27ea7a4ffc4da1
Chimson <crunor55@hotmail.com>
2021-10-07 13:24:03 +0200

    use Card in UnsplashImage

--- src/components/UnsplashImage.tsx
+++ src/components/UnsplashImage.tsx
@@ -1,4 +1,4 @@
-import Image from "react-bootstrap/Image";
+import { Button, Card } from "react-bootstrap";
 import { UnsplashImage } from "../features/images/imagesSlice";
 
 interface Props {
@@ -8,19 +8,23 @@ interface Props {
 export function ImageComponent({ image }: Props) {
   return (
     <div className="col-md-4 mb-2">
-      <Image
-        style={{
-          width: "100%",
-          maxHeight: "200px",
-          minHeight: "200px",
-          objectFit: "cover",
-        }}
-        thumbnail
-        src={image.urls.small}
-        alt={image.alt_description}
-        rounded
-        fluid
-      />
+      <Card>
+        <Card.Img
+          style={{
+            width: "100%",
+            maxHeight: "200px",
+            minHeight: "200px",
+            objectFit: "cover",
+          }}
+          src={image.urls.small}
+          alt={image.alt_description}
+        />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
